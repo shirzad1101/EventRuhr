533e8b1b8c0567d8edb76d418c2d50ed5dbd3e00
shirzad1101 <shirzad.qadri123@gmail.com>
2026-05-26 01:22:09 +0200

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import Home from "./pages/Home";
-import Gallery from "./pages/Gallery";
+import Home from "./pages/Home"; // Sucht im Unterordner 'pages' nach Home.jsx
+import Gallery from "./Gallery";   // Sucht direkt im 'src'-Ordner nach Gallery.jsx
 
 export default function App() {
   return (
