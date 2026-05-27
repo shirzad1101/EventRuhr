c94a9058d186d8d25926a256793e754da12af8bc
shirzad1101 <shirzad.qadri123@gmail.com>
2026-05-27 16:43:41 +0200

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,8 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
 import Gallery from "./Gallery";
-import Danke from "./Danke"; // NEU: Danke-Seite importieren
+import Danke from "./Danke"; 
+import Datenschutz from "./Datenschutz"; // NEU: Datenschutz-Seite importieren
 
 export default function App() {
   return (
@@ -9,7 +10,8 @@ export default function App() {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/gallery" element={<Gallery />} />
-        <Route path="/danke" element={<Danke />} /> {/* NEU: Route für Google Ads */}
+        <Route path="/danke" element={<Danke />} /> {/* Route für Google / Meta Ads Tracking */}
+        <Route path="/datenschutz" element={<Datenschutz />} /> {/* NEU: Rechtssichere Route für das Buchungs-Modal */}
       </Routes>
     </Router>
   );
